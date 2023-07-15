b72f1750e86e88f4ace140586338b17df2f301b9
David Day <david.day2017@gmail.com>
2023-07-15 12:07:21 -0500

    implement programmer_InitSpi/Parallel

--- src/app/programmer_tm4c.c
+++ src/app/programmer_tm4c.c
@@ -1,5 +1,14 @@
 
 #include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include "driverlib/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/ssi.h"
+
+#define PART_TM4C123GH6PM
+#include "driverlib/pin_map.h"
 
 #define DATA_WIDTH 8
 #define ADDRESS_WIDTH 15
@@ -26,5 +35,68 @@ typedef struct _Programmer {
     SpiModule_t spi;
 } Programmer;
 
-int programmer_InitParallel(Programmer *prog);
-int programmer_InitSpi(Programmer *prog);
+static Programmer *Prog;
+
+int programmer_InitParallel(void) {
+    for (uint32_t *port = Prog->ports; *port != 0; port++) {
+        SysCtlPeripheralEnable(*port);
+        while (!SysCtlPeripheralReady(*port))
+            ;
+    }
+
+    GPIOPinTypeGPIOOutput(Prog->WEn.port, Prog->WEn.pin);
+    GPIOPinTypeGPIOOutput(Prog->CEn.port, Prog->CEn.pin);
+    GPIOPinTypeGPIOOutput(Prog->OEn.port, Prog->OEn.pin);
+
+    GPIOPinWrite(Prog->WEn.port, Prog->WEn.pin, Prog->WEn.pin);
+    GPIOPinWrite(Prog->CEn.port, Prog->CEn.pin, Prog->CEn.pin);
+    GPIOPinWrite(Prog->OEn.port, Prog->OEn.pin, Prog->OEn.pin);
+
+    for (int i = 0; i < ADDRESS_WIDTH; i++ ) {
+        GPIOPinTypeGPIOOutput(Prog->A[i].port, Prog->A[i].pin);
+    }    
+
+    return 1;
+}
+
+int programmer_InitSpi(void) {
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(Prog->spi.CLK.port);
+    SysCtlPeripheralEnable(Prog->spi.CS.port);
+    SysCtlPeripheralEnable(Prog->spi.RX.port);
+    SysCtlPeripheralEnable(Prog->spi.TX.port);
+
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
+    GPIOPinConfigure(GPIO_PA3_SSI0FSS);
+    GPIOPinConfigure(GPIO_PA4_SSI0RX);
+    GPIOPinConfigure(GPIO_PA5_SSI0TX);
+
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, 
+                     GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3 | GPIO_PIN_2);
+
+    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, 
+            SSI_MODE_MASTER, 1000000, 8);
+
+    SSILoopbackEnable(SSI0_BASE);
+
+    SSIEnable(SSI0_BASE);
+
+    return 1;
+}
+
+int programmer_EnableIOPins(void);
+int programmer_DisableIOPins(void);
+int programmer_SetAddress(enum AddressBusWidth busWidth, uint32_t address);
+int programmer_SetData(uint32_t data);
+uint32_t programmer_GetData();
+int programmer_Delay100ns(uint32_t delay);
+int programmer_EnableChip(void);
+int programmer_DisableChip(void);
+int programmer_SetSpiClockFreq(enum SpiFrequency freq);
+enum SpiFrequency programmer_GetSpiClockFreq(void);
+int programmer_SetSpiMode(enum SpiMode mode);
+enum SpiMode programmer_GetSpiMode(void);
+int programmer_SpiWrite(const char *buf, size_t count);
+int programmer_SpiRead(const char *txbuf, char *rxbuf, size_t count);
+
