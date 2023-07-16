018bcd028f81fe7dea67f6e498f7cabe5ac62590
David Day <david.day2017@gmail.com>
2023-07-16 16:22:49 -0500

    remove obsolete header files, clean up main

--- inc/spi.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SPI_H__
-#define __SPI_H__
-
-#include <stddef.h>
-
-int spi_transmit(const char *txbuf, char *rxbuf, size_t count);
-
-#endif /* __SPI_H__ */
-
--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/hw_memmap.h"
+#include "string.h"
 
 int main(void){
     while (1)
