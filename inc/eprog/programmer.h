a669f6ffa6b04deb2db59e5258a410333e5186d0
David Day <david.day2017@gmail.com>
2023-07-16 16:38:02 -0500

    remove circular dependency between eprog.h and programmer.h

--- inc/eprog/programmer.h
+++ inc/eprog/programmer.h
@@ -2,21 +2,21 @@
 #define __PROGRAMMER_H___
 
 #include <stdint.h>
-#include "eprog.h"
+#include <stddef.h>
 
 int programmer_InitParallel(void);
 int programmer_InitSpi(void);
 int programmer_EnableIOPins(void);
 int programmer_DisableIOPins(void);
-int programmer_SetAddress(enum AddressBusWidth busWidth, uint32_t address);
+int programmer_SetAddress(uint8_t busWidth, uint32_t address);
 int programmer_SetData(uint32_t data);
 uint32_t programmer_GetData(void);
 int programmer_Delay100ns(uint32_t delay);
 int programmer_EnableChip(void);
 int programmer_DisableChip(void);
-int programmer_SetSpiClockFreq(enum SpiFrequency freq);
+int programmer_SetSpiClockFreq(uint32_t freq);
 enum SpiFrequency programmer_GetSpiClockFreq(void);
-int programmer_SetSpiMode(enum SpiMode mode);
+int programmer_SetSpiMode(uint8_t mode);
 enum SpiMode programmer_GetSpiMode(void);
 int programmer_SpiWrite(const char *buf, size_t count);
 int programmer_SpiRead(const char *txbuf, char *rxbuf, size_t count);
