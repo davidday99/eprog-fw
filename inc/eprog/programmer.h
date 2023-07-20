cf566493483f6526df09f9bf834bcdd5b31fbffd
David Day <david.day2017@gmail.com>
2023-07-19 23:04:40 -0500

    minor reordering

--- inc/eprog/programmer.h
+++ inc/eprog/programmer.h
@@ -22,8 +22,8 @@ int programmer_SetSpiClockFreq(uint32_t freq);
 uint32_t programmer_GetSpiClockFreq(void);
 int programmer_SetSpiMode(uint8_t mode);
 uint8_t programmer_GetSpiMode(void);
-int programmer_SpiWrite(const char *buf, size_t count);
 int programmer_SpiRead(const char *txbuf, char *rxbuf, size_t count);
+int programmer_SpiWrite(const char *buf, size_t count);
 
 #endif /* __PROGRAMMER_H__ */
 
