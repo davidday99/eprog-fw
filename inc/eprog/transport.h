f15e61cc70a2d7db8c067fd27b3bfecbc6f58908
David Day <david.day2017@gmail.com>
2023-07-23 00:31:41 -0500

    add transport interface

---
+++ inc/eprog/transport.h
@@ -0,0 +1,10 @@
+#ifndef __TRANSPORT_H__
+#define __TRANSPORT_H__
+
+#include <stddef.h>
+
+int transport_getData(char *in, size_t count); 
+int transport_putData(const char *out, size_t count); 
+
+#endif /* __TRANSPORT_H__ */
+
